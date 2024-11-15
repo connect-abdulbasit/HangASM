fdf697dfdfdf72583a1be4654719dd39a32389f1
Abdul Basit <connect.abdulbasit.cs@gmail.com>
2024-11-16 02:53:18 +0500

    modification in the code

--- Hangman-COAL-Project/Hangman-COAL-Project/Source.asm
+++ Hangman-COAL-Project/Hangman-COAL-Project/Source.asm
@@ -2,14 +2,14 @@ include irvine32.inc
 .data
 
 choice BYTE "What do you want ? ",0
-choice1 BYTE "1.  play ",0
-choice2 BYTE "2.  how to play",0
-choice3 BYTE "3.  exit ",0
+choice1 BYTE "1. Play ",0
+choice2 BYTE "2. How to play",0
+choice3 BYTE "3. Exit ",0
 
 msg1 BYTE "You exit the game successfully Thank you ",0
 msg2 BYTE "Do you want to play again ?",0
 
-choice4 BYTE "1.yes",0
+choice4 BYTE "1.Yes",0
 choice5 BYTE "2.No",0
 
 msg3 BYTE "------------WLECOME TO THE HANGMAN GAME --------------------",0
@@ -28,13 +28,19 @@ msg15 BYTE " After the game ends, your final score will be calculated based on t
 msg16 BYTE " -------------------------- Tips ------------------------:",0
 msg17 BYTE " Keep track of the letters you've guessed to avoid repeating guesses.",0
 msg18 BYTE " Good luck, and have fun !",0
-
+endingPrompt BYTE "Press any key to continue..."
 select_option DWORD ?
 
+
+GuessWord1 BYTE "ORANGE",0, "FAMILY",0, "SILVER",0, "DONATE",0, "MONDAY",0, "NATURE",0, "BROKEN",0, "RACHEL",0, "FRIDAY",0, "FATHER", 0
+GuessWord2 BYTE "JASMINE",0, "OSTRICH",0, "CHAPTER",0, "CRYSTAL",0, "DEPOSIT",0, "HOLIDAY",0, "CALIBER",0, "KITCHEN", 0,"KINGDOM",0, "ACQUIRE", 0
+GuessWord3 BYTE "DINOSAUR",0,"PROVIDES",0,"DYNAMICS",0,"PRODUCES",0,"ALPHABET",0,"UNDERWAY",0,"UNIFORMS",0,"UNIQUELY",0,"THURSDAY",0,"ANTIHERO",0
+GuessWord4 BYTE "DANGEROUS",0, "MASCULINE",0, "SOMETHING",0, "KNOWLEDGE",0, "WRESTLING",0, "WONDERFUL", 0, "EDUCATION",0, "CELEBRATE",0, "ALONGSIDE",0, "COMPANIES", 0
 
 .code
 main proc 
 
+mainMenu:
 mov edx , offset choice
 call writestring
 call crlf
@@ -61,7 +67,8 @@ je play
 cmp select_option , 2
 je how_to_play
 
-jmp endprogram
+cmp select_option , 3
+je endprogram
 
 play:
 call function_play
@@ -83,11 +90,13 @@ mov select_option , eax
 
 cmp select_option , 1
 je play
-jmp endprogram
+jmp skip
 
 how_to_play:
 call function_howtoplay
-jmp endprogram
+
+skip:
+jmp mainMenu
 
 endprogram:
 mov edx , offset msg1
@@ -167,12 +176,13 @@ call writestring
 call crlf
 call crlf
 
-
+mov edx,offset endingPrompt
+call writestring
+call readdec
 ret 
 function_howtoplay endp
 
 function_play proc
-
 
 ret
 function_play endp
